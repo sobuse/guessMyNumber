8e890ce1688e7a7cc9f6c7e267c9d07353b3ef20
sammy <ehuerikenbaba@gmail.com>
2021-05-28 22:01:52 +0100

    implimenting game logic

--- script.js
+++ script.js
@@ -5,12 +5,26 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
 
+const secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function (){
     const guess = Number(document.querySelector('.guess').value);
 
     if(!guess){
         document.querySelector('.message').textContent = ' 🚩 Please input a Number';
+    } else if(guess === secretNumber ){
+        document.querySelector('.message').textContent = ' Correct Number!';
+    } else if(guess > secretNumber){
+        document.querySelector('.message').textContent = ' Too High!';
+        score--;
+        document.querySelector('.score').textContent = score;
 
+    } else if (guess < secretNumber){
+        document.querySelector('.message').textContent = ' Too Low';
+        score--;
+        document.querySelector('.score').textContent = score;
 
     }
 });
