daeabc8c5c7aa5b3cb6be9323fa9b65d72ea4d18
sammy <ehuerikenbaba@gmail.com>
2021-05-28 21:33:09 +0100

    implimented where guess is empty

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = ' Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+document.querySelector('.check').addEventListener('click', function (){
+    const guess = Number(document.querySelector('.guess').value);
+
+    if(!guess){
+        document.querySelector('.message').textContent = ' 🚩 Please input a Number';
+
+
+    }
+});
